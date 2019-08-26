ca0f136be75468cf08a3c518e5202444e3e61fb5
Till Friebe <friebetill@gmail.com>
2019-08-26 12:35:36 +0200

    Add french locale

---
+++ lib/src/locales/fr.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2016, rinukkusu. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import '../localedata.dart';
+import '../identifier_position.dart';
+
+class LocaleFr implements ILocaleData {
+  String get seconds => 'quelques secondes';
+
+  String get aMinute => 'une minute';
+  String get minutes => '%i minutes';
+
+  String get anHour => 'une heure';
+  String get hours => '%i heures';
+
+  String get aDay => 'un jour';
+  String get days => '%i jours';
+
+  String get aMonth => 'un mois';
+  String get months => '%i mois';
+
+  String get aYear => 'un an';
+  String get years => '%i ans';
+
+  String get futureIdentifier => 'en';
+  String get pastIdentifier => 'il y a';
+
+  IdentifierPosition get futurePosition => IdentifierPosition.prepend;
+  IdentifierPosition get pastPosition => IdentifierPosition.prepend;
+}
