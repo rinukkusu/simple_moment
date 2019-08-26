3292bd87b9f238f5ab44709d0fa5220c24d1d82e
Till Friebe <friebetill@gmail.com>
2019-08-26 12:29:31 +0200

    Add italian locale

--- lib/simple_moment.dart
+++ lib/simple_moment.dart
@@ -8,5 +8,6 @@ export 'src/localedata.dart';
 export 'src/locales/de.dart';
 export 'src/locales/en.dart';
 export 'src/locales/es.dart';
+export 'src/locales/it.dart';
 export 'src/locales/pt_br.dart';
 export 'src/simple_moment_base.dart';
---
+++ lib/src/locales/it.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2016, rinukkusu. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import '../localedata.dart';
+import '../identifier_position.dart';
+
+class LocaleIt implements ILocaleData {
+  String get seconds => 'un secondo';
+
+  String get aMinute => 'un minuto';
+  String get minutes => '%i minuti';
+
+  String get anHour => 'un \'ora';
+  String get hours => '%i ore';
+
+  String get aDay => 'un giorno';
+  String get days => '%i giorni';
+
+  String get aMonth => 'un mese';
+  String get months => '%i mesi';
+
+  String get aYear => 'un anno';
+  String get years => '%i anni';
+
+  String get futureIdentifier => 'tra';
+  String get pastIdentifier => 'fa';
+
+  IdentifierPosition get futurePosition => IdentifierPosition.prepend;
+  IdentifierPosition get pastPosition => IdentifierPosition.prepend;
+}
+
