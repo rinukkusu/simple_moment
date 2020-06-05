793c6eb9cef2c6011857fea0d6daf765f786995d
Steve Nosse <nossesteve@gmail.com>
2020-06-05 16:47:21 +0100

    added tests for get quarter and add date + months

--- test/simple_moment_test.dart
+++ test/simple_moment_test.dart
@@ -46,20 +46,20 @@ void main() {
       );
     });
 
-    test('Moment.add(years:1)', () {
+    test('Moment.add(years:1, months: 4, days: 6)', () {
       DateTime currentDateString = DateTime.parse("2020-06-03 11:25:28.001");
       Moment currentDate = Moment.fromDate(currentDateString);
       expect(
-        currentDate.add(years: 1).compareTo(DateTime.parse("2021-06-03 11:25:28.001")) == 0,
+        currentDate.add(years: 1, months: 4, days: 6).compareTo(DateTime.parse("2021-10-09 11:25:28.001")) == 0,
         isTrue,
       );
     });
 
-    test('Moment.substract(years:1)', () {
+    test('Moment.substract(years:1, months: 2, days: 3)', () {
       DateTime currentDateString = DateTime.parse("2020-06-03 11:25:28.001");
       Moment currentDate = Moment.fromDate(currentDateString);
       expect(
-        currentDate.subtract(years: 1).compareTo(DateTime.parse("2019-06-03 11:25:28.001")) == 0,
+        currentDate.subtract(years: 1, months: 2, days: 3).compareTo(DateTime.parse("2019-03-31 11:25:28.001")) == 0,
         isTrue,
       );
     });
@@ -67,6 +67,12 @@ void main() {
     test('Moment.format(pattern)', () {
       Moment currentDate = Moment.fromDate(DateTime.parse("2020-06-03 11:25:28.001"));
       expect(currentDate.format("yyyy-MM-dd HH:mm").compareTo("2020-06-03 11:25") == 0, isTrue);
+    });
+
+    test("get quarter", (){
+      Moment currentDate = Moment.fromDate(DateTime.parse("2020-06-03 11:25:28.001"));
+
+      expect(currentDate.quarter == 2, isTrue);
     });
   });
 }
