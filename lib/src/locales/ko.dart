f17bb0bdbd653d46555f97802f0ceeae15287935
pekosong <pekopeko11@gmail.com>
2021-05-14 07:04:52 +0000

    class name changed

--- lib/src/locales/ko.dart
+++ lib/src/locales/ko.dart
@@ -4,11 +4,11 @@
 import '../localedata.dart';
 import '../identifier_position.dart';
 
-class LocaleEn implements ILocaleData {
+class LocaleKo implements ILocaleData {
   final String _localeString;
   String get localeString => _localeString;
 
-  LocaleEn([this._localeString = "ko"]);
+  LocaleKo([this._localeString = "ko"]);
 
   String get seconds => '몇 초';
 
